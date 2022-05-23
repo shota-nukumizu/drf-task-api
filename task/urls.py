801dc7d1d5f0a6b72b9ee16bfc505079b2bf21b1
shota-nukumizu <author2000.1225@gmail.com>
2022-05-23 14:54:12 +0900

    add task-route

--- task/urls.py
+++ task/urls.py
@@ -4,8 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
     TokenRefreshView,
 )
+from . import views
 
 urlpatterns = [
+    path('', views.TaskViewSet, name='tasks'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
