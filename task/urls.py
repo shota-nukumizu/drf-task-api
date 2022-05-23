648cf364090024f2a7d73868cc9de2df4dfff0c8
shota-nukumizu <author2000.1225@gmail.com>
2022-05-23 15:09:11 +0900

    create taskapi:delete authentication and modified url

--- task/urls.py
+++ task/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenVerifyView,
     TokenRefreshView,
 )
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('tasks', views.TaskViewSet)
 
 urlpatterns = [
-    path('', views.TaskViewSet, name='tasks'),
+    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
--- task/views.py
+++ task/views.py
@@ -1,12 +1,10 @@
 from requests import Response
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
 
 from .serializers import TaskSerializer
 from .models import Task
 
 
 class TaskViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAdminUser]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
