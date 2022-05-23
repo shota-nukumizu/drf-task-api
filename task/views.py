b276bb70506174414e1413f3eb94d607d06141fe
shota-nukumizu <author2000.1225@gmail.com>
2022-05-23 14:52:51 +0900

    create task serializer

--- task/views.py
+++ task/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from requests import Response
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .serializers import TaskSerializer
+from .models import Task
+
+
+class TaskViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
